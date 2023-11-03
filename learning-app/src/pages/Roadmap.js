ea0dcdb7bf3effb7902850abf47a296edaebfa1e
Laura W <142807355+ylw4@users.noreply.github.com>
2023-11-03 18:24:26 -0400

    added progress bar to roadmap

--- learning-app/src/pages/Roadmap.js
+++ learning-app/src/pages/Roadmap.js
@@ -1,14 +1,41 @@
 import './Roadmap.css';
-
+import ProgressBar from 'react-bootstrap/ProgressBar';
+// User information 
+const user = {
+    name: 'Name',
+    path: 1,
+    progress: 50,
+    rewards: 50,
+}
+// Page
 function Roadmap() {
+    let content;
+    // Display different information if user is not logged in
+    if (isLoggedIn){
+        content = <Main_Roadmap />
+    } else {
+        // Add login form component
+    }
     return(
-        <div className="Roadmap">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                ROADMAP PAGE.
-            </p>
+        <div>
+            content
         </div>
     );
+}
+
+function Main_Roadmap(){
+    <div className="Roadmap">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to Path {user.path}</h2>
+        <p>
+            ROADMAP PAGE.
+        </p>
+    </div>
+}
+
+// Progress bar
+function RewardBar(){
+    return <ProgressBar now={user.progress} label={`${user.progress}%`} />;
 }
 
 export default Roadmap;
