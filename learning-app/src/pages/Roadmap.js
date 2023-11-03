3c710eecb3fdc8a44edef330e0e1ce84a841a811
Elijah Paulman <eli.paulman@gmail.com>
2023-11-03 18:20:54 -0400

    added navbar

--- learning-app/src/pages/Roadmap.js
+++ learning-app/src/pages/Roadmap.js
@@ -1,14 +1,13 @@
-import './Roadmap.css';
-
-function Roadmap() {
-    return(
-        <div className="Roadmap">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                ROADMAP PAGE.
-            </p>
+import React from 'react';
+import { Nav } from '../components/Navbar/NavbarElements';
+ 
+const Roadmap = () => {
+    return (
+        
+        <div>
+            <h1>Sign Up</h1>
         </div>
     );
-}
-
+};
+ 
 export default Roadmap;
