57c6617180274bc04614929053e4a4a86e31c431
nicholasyfu1 <nicholasyfu1@gmail.com>
2023-11-04 02:10:49 -0400

    score page routing

--- learning-app/src/pages/Score.js
+++ learning-app/src/pages/Score.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Score.js';
+import {Link } from "react-router-dom";
 
 function Score(props) {
     return (
         <div>
             <h1>Score: {props.score}</h1>
+            <Link to="/Roadmap">
+                <button class="button">Continue to Roadmap</button>
+            </Link>
         </div>
     );
 }
