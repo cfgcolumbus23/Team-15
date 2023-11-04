14bcb2384648f25884e2f9c0a5c0acf720c0f7c6
Najib <52837430+najchris11@users.noreply.github.com>
2023-11-04 01:54:56 -0400

    General cleanup

--- learning-app/src/App.js
+++ learning-app/src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import {createNote} from "./Test"
 import Home from "./pages/Homepage";
 import Roadmap from "./pages/Roadmap";
 import Login from "./pages/Login";
