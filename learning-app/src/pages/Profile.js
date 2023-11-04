8a39510791ef014bc071c6092d94265cd6e34393
Tina-help <tinalin036@gmail.com>
2023-11-04 11:29:59 -0400

    Update Profile.js

--- learning-app/src/pages/Profile.js
+++ learning-app/src/pages/Profile.js
@@ -1,115 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Profile.css";
 import { auth } from "./../Connection";
 import { getDatabase, ref, child, get } from "firebase/database";
 
-const dbRef = ref(getDatabase());
-let userId = auth.currentUser.email.substring(
-  0,
-  auth.currentUser.email.indexOf("@")
-);
+// const dbRef = ref(getDatabase());
+// let userId = auth.currentUser?.email.substring(
+//   0,
+//   auth.currentUser.email.indexOf("@")
+// );
 
-console.log(userId);
-get(child(dbRef, "users")).then((snapshot) => {
-  class Profile extends React.Component {
-    constructor(props) {
-      super(props);
-      if (snapshot.exists()) {
-        var user = snapshot[userId];
+function Profile() {
+  const [user, setUser] = useState({});
 
-        this.state = {
-          firstName: user["firstName"],
-          lastName: user["lastName"],
-          age: user["age"],
-          email: user["email"],
-          phoneNumber: user["phoneNumber"],
-          points: 1500, // hard-coded points
-        };
-      }
+  useEffect(() => {
+    // get(child(dbRef, "users")).then((snapshot) => {
+    //   if (snapshot.exists()) {
+    //     console.log(snapshot);
+    //     var user = snapshot[userId];
 
-      return (
-        <div className="profile-container">
-          <h1>
-            {this.state.firstName} {this.state.lastName}
-          </h1>
-          <p>Age: {this.state.age}</p>
-          <p>Email: {this.state.email}</p>
-          <p>Phone: {this.state.phoneNumber}</p>
-          <p>Points: {this.state.points}</p>
-        </div>
-      );
-    }
-    // render() {
+    //     setUser(...user, { firstName: user["firstName"] });
+    //     setUser(...user, { lastName: user["lastName"] });
+    //     setUser(...user, { age: user["age"] });
+    //     setUser(...user, { email: user["email"] });
+    //     setUser(...user, { phoneNumber: user["phoneNumber"] });
+    //     setUser(...user, { points: 1500 });
+    //   } else {
 
+    const object = {
+      firstName: "John",
+      lastName: "Doe",
+      age: 30,
+      email: "user not found",
+      phoneNumber: "123-456-7890",
+      points: 1500,
+    };
+
+    setUser(object);
     // }
-  }
-});
-//       } else {
-//         class Profile extends React.Component {
-//           constructor(props) {
-//             super(props);
-//             this.state = {
-//               firstName: "John",
-//               lastName: "Doe",
-//               age: 30,
-//               email: "user not found",
-//               phoneNumber: "123-456-7890",
-//               points: 1500, // hard-coded points
-//             };
-//           }
+    // });
+  }, []);
 
-//           render() {
-//             return (
-//               <div className="profile-container">
-//                 <h1>
-//                   {this.state.firstName} {this.state.lastName}
-//                 </h1>
-//                 <p>Age: {this.state.age}</p>
-//                 <p>Email: {this.state.email}</p>
-//                 <p>Phone: {this.state.phoneNumber}</p>
-//                 <p>Points: {this.state.points}</p>
-//               </div>
-//             );
-//           }
-//         }
-//       }
-//     }
-//     console.log(snapshot.val());
-//   } else {
-//     console.log("No data available");
-//   }
-// })
-// .catch((error) => {
-//   console.error(error);
-// });
-
-// //----------Uncomment later if not working
-// class Profile extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       firstName: "John",
-//       lastName: "Doe",
-//       age: 30,
-//       email: "user not found",
-//       phoneNumber: "123-456-7890",
-//       points: 1500, // hard-coded points
-//     };
-//   }
-
-//   render() {
-//     return (
-//       <div className="profile-container">
-//         <h1>
-//           {this.state.firstName} {this.state.lastName}
-//         </h1>
-//         <p>Age: {this.state.age}</p>
-//         <p>Email: {this.state.email}</p>
-//         <p>Phone: {this.state.phoneNumber}</p>
-//         <p>Points: {this.state.points}</p>
-//       </div>
-//     );
-//   }
-// }
+  return (
+    <div className="profile-container">
+      <h1>
+        {user.firstName} {user.lastName}
+      </h1>
+      <p>Age: {user.age}</p>
+      <p>Email: {user.email}</p>
+      <p>Phone: {user.phoneNumber}</p>
+      <p>Points: {user.points}</p>
+    </div>
+  );
+}
 
 export default Profile;
