9fb86e4a6529395df9bb8ba788adc740cbe557ab
Najib <52837430+najchris11@users.noreply.github.com>
2023-11-03 21:28:18 -0400

    Merge branch 'main' of https://github.com/cfgcolumbus23/Team-15

--- learning-app/src/App.js
+++ learning-app/src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import {createNote} from "./Test"
 import Home from "./pages/Homepage";
 import Roadmap from "./pages/Roadmap";
 import Login from "./pages/Login";
 function App() {
+  useEffect(()=>{
+    createNote()
+  })
   return (
     <Router>
       <Navbar />
--- learning-app/src/pages/Roadmap.js
+++ learning-app/src/pages/Roadmap.js
@@ -50,14 +50,8 @@ function Roadmap() {
         content = <Main_Roadmap />;
     }
     return(
-        <> 
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-            <div class="module-container">
-            {data.Path1?.map((item, index)=><div class="module" id={`module-${index}`}>
-                <span class="material-symbols-outlined">{(item.complete == 'True') ? 'check_box' :'check_box_outline_blank'}</span>
-                <p>{item.moduleName}</p>
-                <p>Reward: ${item.reward}</p>
-                </div>)}
+        <div class="module-container">
+            {data.Path1?.map((item, index)=><div class="module" id={`module-${index}`}><p>{item.moduleName}</p> <p>Reward: ${item.reward}</p></div>)}
             {content}
         </div>
         </> 
