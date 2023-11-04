aefb69f52e6174871f4be4d687f76f16b018e7ff
nicholasyfu1 <nicholasyfu1@gmail.com>
2023-11-03 23:53:12 -0400

    Selection paths

--- learning-app/src/pages/Selection.js
+++ learning-app/src/pages/Selection.js
@@ -38,10 +38,45 @@ function Selection() {
         </div>
         <div className='selection-container'>
             <div id='selection-column-1' className='selection-column'>
+                <div className='selection-column-title'>
+                    <h3>IT Helpdesk Path</h3>
+                </div>
+                <div className='selection-column-desc'>
+                    <p>
+                        The IT Helpdesk Path is designed to prepare learners for
+                        roles that involve providing technical support and
+                        troubleshooting services to end-users. This path combines
+                        customer service skills with technical expertise to address
+                        computer software, hardware, and network issues.
+                    </p>
+                </div>
             </div>
             <div id='selection-column-2' className='selection-column'>
+                <div className='selection-column-title'>
+                    <h3>Cybersecurity Path</h3>
+                </div>
+                <div className='selection-column-desc'>
+                    <p>
+                        The IT Security Path is designed to equip learners with a
+                        robust understanding of information technology security
+                        principles. This path covers a broad spectrum of IT
+                        security aspects, including network security, data
+                        protection, and compliance standards.  
+                    </p>
+                </div>
             </div>
             <div id='selection-column-3' className='selection-column'>
+                <div className='selection-column-title'>
+                    <h3>Web Development Path</h3>
+                </div>
+                <div className='selection-column-desc'>
+                    <p>
+                    The Web Development Path is a comprehensive journey through
+                    the world of building and maintaining web applications. From
+                    frontend to backend, this path teaches the essential skills
+                    needed to develop dynamic and responsive websites.
+                    </p>
+                </div>
             </div>
         </div>
         </>
