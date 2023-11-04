2ddd6264cc07e6e34ba2c482fc646fb7fd5fe823
Elijah Paulman <eli.paulman@gmail.com>
2023-11-03 21:11:27 -0400

    final navbar

--- learning-app/src/components/Navbar/index.js
+++ learning-app/src/components/Navbar/index.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { Nav, NavLink, NavMenu, Bars } from "./NavbarElements";
 import { Link } from "react-router-dom";
-import Logo from "./logo"; 
+import Logo from "./logo"; // Import the Logo component
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(window.innerWidth > 768);
 
   const toggleMenu = () => {
-    setShowMenu(!showMenu);
+    if (window.innerWidth <= 768) {
+      setShowMenu(!showMenu);
+    }
   };
 
   useEffect(() => {
@@ -29,17 +31,29 @@ const Navbar = () => {
   return (
     <>
       <Nav>
-        <Logo /> 
+        <Logo />
         <Bars onClick={toggleMenu} />
         {showMenu && (
           <NavMenu>
-            <NavLink onClick={toggleMenu} to="/Homepage" activeStyle={{ color: "#4d4dff" }}>
+            <NavLink
+              onClick={toggleMenu}
+              to="/Homepage"
+              activeStyle={{ color: "#4d4dff" }}
+            >
               Homepage
             </NavLink>
-            <NavLink onClick={toggleMenu} to="/Roadmap" activeStyle={{ color: "#4d4dff" }}>
+            <NavLink
+              onClick={toggleMenu}
+              to="/Roadmap"
+              activeStyle={{ color: "#4d4dff" }}
+            >
               Roadmap
             </NavLink>
-            <NavLink onClick={toggleMenu} to="/Login" activeStyle={{ color: "#4d4dff" }}>
+            <NavLink
+              onClick={toggleMenu}
+              to="/Login"
+              activeStyle={{ color: "#4d4dff" }}
+            >
               Login
             </NavLink>
           </NavMenu>
