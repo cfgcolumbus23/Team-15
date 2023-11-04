85151092c3457383046049ff0bec0bbeb265d0ea
nicholasyfu1 <nicholasyfu1@gmail.com>
2023-11-03 22:01:31 -0400

    Working on roadmap and selection

--- learning-app/src/pages/Selection.js
+++ learning-app/src/pages/Selection.js
@@ -3,10 +3,40 @@ import { Nav } from '../components/Navbar/NavbarElements';
 import './Roadmap.css';
 
 function Selection() {
+
+    {/* Read in JSON File */}
+    const [data,setData]=useState([]);
+    const getData=()=>{
+        fetch('mock.json'
+        ,{
+          headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+           }
+        }
+        )
+          .then(function(response){
+            {/* Console log */}
+            console.log(response)
+            return response.json();
+          })
+          .then(function(mockJson) {
+            setData(mockJson);
+          });
+      }
+      useEffect(()=>{
+        getData()
+      },[])
+
+    {/* Everything Else */}
+
+
     return (
+        <>
         <div className='selection'>
             <h2>Selection Page: 3 columns of paths</h2>
         </div>
+        </>
     );
 }
 export default Selection;
