07ffa00fa20fd8c96a49e6330df3fae4bd8bff7e
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2023-11-04 01:58:44 -0400

    switch functionality

--- learning-app/src/pages/Login.js
+++ learning-app/src/pages/Login.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../Connection"; // Assuming Connection.js is in the src folder, outside of the pages folder.
 import { signInWithEmailAndPassword } from "firebase/auth"; // Corrected the import statement.
+import { useNavigate } from 'react-router-dom';
 
 //Call the backend to authenticate the login information
 //have it return a success or failure
@@ -11,15 +12,8 @@ function AuthenticateLoginInformation(login) {
   return signInWithEmailAndPassword(auth, login.username, login.password);
 }
 
-function HandleResult(success) {
-  if (success) {
-    //switch to the new screen that says Success!
-    console.log("Success");
-  } else {
-    //display an error message
-    console.log("Failure");
-  }
-}
+
+
 
 function UsernameInput(login, setLogin) {
   return (
@@ -54,12 +48,24 @@ function PasswordInput(login, setLogin) {
   );
 }
 
-//Store the username and password
 function LoginForm() {
   const [login, setLogin] = useState({
     username: "",
     password: "",
   });
+  const [error, setError] = useState(""); // Add state for the error message
+  const navigate = useNavigate(); // Hook for navigation
+  
+  function handleResult(success, errorMessage = "") {
+    if (true) {
+      // Navigate to the roadmap page
+      navigate('/roadmap'); // Make sure to replace '/roadmap' with your actual route
+    } else {
+      console.log("Failure", errorMessage);
+      setError(errorMessage); // Update the error state with the error message
+    }
+  }
+  // Use this inside LoginForm to access the navigate function
 
   //Saves the username and password
   //Authenticates it
@@ -69,35 +75,35 @@ function LoginForm() {
     console.log(login);
     AuthenticateLoginInformation(login)
       .then((userCredential) => {
-        // User is signed in
-        HandleResult(true);
+        handleResult(true); // Call handleResult on success
       })
       .catch((error) => {
         // Handle Errors here.
         const errorCode = error.code;
         const errorMessage = error.message;
         console.log(errorCode, errorMessage);
-        HandleResult(false);
+        handleResult(false, errorMessage); // Pass the error message to handleResult
       });
     setLogin({ username: "", password: "" }); //clear Data
   };
 
+  
+
   // clean up later into smaller functions
   return (
     <div className="login-Page">
-      <form className="login-form" onSubmit={handleSubmit}>
-        <h2>Login</h2>
-        {UsernameInput(login, setLogin)}
-        {PasswordInput(login, setLogin)}
-        <button type="submit">Login</button>
-      </form>
-
-      {/* //Might Change later */}
-      <Link to="/SignUp">
-        <button>Sign Up</button>
-      </Link>
-    </div>
-  );
+    <form className="login-form" onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      {error && <p className="error-message">{error}</p>} {/* Display error message */}
+      {UsernameInput(login, setLogin)}
+      {PasswordInput(login, setLogin)}
+      <button type="submit">Login</button>
+    </form>
+    <Link to="/SignUp">
+      <button>Don't have an account? Sign up here</button>
+    </Link>
+  </div>
+);
 }
 
 //Needs to add a submit button
