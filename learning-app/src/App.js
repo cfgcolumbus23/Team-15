d80fc21590dd4fa2f25b4bfe24ef5c0d30384fa6
Laura W <142807355+ylw4@users.noreply.github.com>
2023-11-04 02:35:43 -0400

    added to mycourses page

--- learning-app/src/App.js
+++ learning-app/src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import { RewardBar } from './components/Rewardbar/RewardbarElement';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import {createNote} from "./Test"
 import Home from "./pages/Homepage";
@@ -9,6 +8,8 @@ import Roadmap from "./pages/Roadmap";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Selection from "./pages/Selection";
+import Mycourses from "./pages/Mycourses";
+
 function App() {
   // useEffect(()=>{
   //   createNote()
@@ -22,8 +23,8 @@ function App() {
         <Route path="/Login" element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/selection" element={<Selection />} />
+        <Route path="/Mycourses" element={<Mycourses />} />
       </Routes>
-      <RewardBar />
     </Router>
   );
 }
