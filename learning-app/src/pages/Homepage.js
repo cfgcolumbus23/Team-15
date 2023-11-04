787b083ffb1b9267a8b3cf4a2efdf00b264c4e71
Sammy Aidja <sam.aidja@gmail.com>
2023-11-04 00:08:28 -0400

    setup tailwindcss and nav bar

--- learning-app/src/pages/Homepage.js
+++ learning-app/src/pages/Homepage.js
@@ -3,12 +3,18 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
+        <div class="loginButton">
             <h1>Ctrl+R</h1>
             <a href="./RoadMap.js">
             <button class = "button">Get Started</button>
             </a>
+
+            <div class = "module">
+              <h1>Hey</h1>
+            </div>
         </div>
+
+        
 
         
     );
