2b9e7fb95484c12e28ef964f6a86104a655ec944
nicholasyfu1 <nicholasyfu1@gmail.com>
2023-11-04 00:54:55 -0400

    assessment

--- learning-app/src/pages/Assessment.js
+++ learning-app/src/pages/Assessment.js
@@ -9,7 +9,7 @@ function Assessment() {
 
     const [data,setData]=useState([]);
     const getData=()=>{
-        fetch('mock.json'
+        fetch('quiz.json'
         ,{
         headers : { 
             'Content-Type': 'application/json',
@@ -31,43 +31,34 @@ function Assessment() {
     },[])
 
     {/* Process Data into Content HTML blocks */}
-    
+
+
+
     return (
         <div class="quiz-container">
-            <h1>Quiz Title</h1>
+            <h1>Initial Assessment</h1>
             <div class="quiz-questions">
                 <form>
                     <ol>
-                        <li>
-                            <h3>Question 1</h3>
-                            <div>
-                                <input type="radio" name="question1" id="q1a" value="a" />
-                                <label for="q1a">Option A</label>
-                            </div>
-                            <div>
-                                <input type="radio" name="question1" id="q1b" value="b" />
-                                <label for="q1b">Option B</label>
-                            </div>
-                            <div>
-                                <input type="radio" name="question1" id="q1c" value="c" />
-                                <label for="q1c">Option C</label>
-                            </div>
-                        </li>
-                        <li>
-                            <h3>Question 2</h3>
-                            <div>
-                                <input type="radio" name="question2" id="q2a" value="a" />
-                                <label for="q2a">Option A</label>
-                            </div>
-                            <div>
-                                <input type="radio" name="question2" id="q2b" value="b" />
-                                <label for="q2b">Option B</label>
-                            </div>
-                            <div>
-                                <input type="radio" name="question2" id="q2c" value="c" />
-                                <label for="q2c">Option C</label>
-                            </div>
-                        </li>
+                    {data.map((item, index)=><li class="quiz-q" id={`q${index}`}>
+                        <h3>{item[0]}</h3>
+                        <div>
+                            <input type="radio" name="question1" id={`q${index}a`} value="a" />
+                            <label for={`q${index}a`}>{item[1][0]}</label>
+                        </div>
+                        <div>
+                            <input type="radio" name="question1" id={`q${index}b`} value="b" />
+                            <label for={`q${index}a`}>{item[1][1]}</label>
+                        </div>
+                        <div>
+                            <input type="radio" name="question1" id={`q${index}c`} value="c" />
+                            <label for={`q${index}a`}>{item[1][2]}</label>
+                        </div>
+                        <div>
+                            <input type="radio" name="question1" id={`q${index}d`} value="d" />
+                            <label for={`q${index}a`}>{item[1][3]}</label>
+                        </div>
+                    </li>)}
                     </ol>
                     <button type="submit">Submit Answers</button>
                 </form>
