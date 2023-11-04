b46cd9e2f39fcb07feb169c95563c32c30eff5b7
nicholasyfu1 <nicholasyfu1@gmail.com>
2023-11-03 21:46:17 -0400

    Created seelctin page

---
+++ learning-app/src/pages/Selection.js
@@ -0,0 +1,12 @@
+import React,{useState,useEffect} from 'react';
+import { Nav } from '../components/Navbar/NavbarElements';
+import './Roadmap.css';
+
+function Selection() {
+    return (
+        <div className='selection'>
+            <h2>Selection Page: 3 columns of paths</h2>
+        </div>
+    );
+}
+export default Selection;
