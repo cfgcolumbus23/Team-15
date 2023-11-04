0a91787b07050aa90b37f24df677ba2fcb8e5aa2
Najib <52837430+najchris11@users.noreply.github.com>
2023-11-04 01:54:56 -0400

    General cleanup

--- learning-app/src/pages/SignUp.js
+++ learning-app/src/pages/SignUp.js
@@ -31,12 +31,10 @@ function SendUserDataToDataBase(profileData) {
   createUserWithEmailAndPassword(auth, profileData.email, profileData.password).then((userCredential) => {
     // Signed up 
     const user = userCredential.user;
-    // ...
   })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
-    // ..
   });
   console.log("Sent");
   return true;
