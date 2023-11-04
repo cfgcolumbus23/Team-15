ee334fabdd10256f61cf357c6834a82869bd3cad
Tina-help <tinalin036@gmail.com>
2023-11-04 00:01:48 -0400

    Added the inputs for all paramters of : first name last name email phone number Date of birth username password

--- learning-app/src/pages/SignUp.js
+++ learning-app/src/pages/SignUp.js
@@ -20,6 +20,160 @@ function HandleResult(success) {
   }
 }
 
+function CreateUsername(profileData, setProfileData) {
+  return (
+    <label>
+      Username:
+      <input
+        type="text"
+        className="inputBox"
+        value={profileData.username}
+        onChange={(e) =>
+          setProfileData({ ...profileData, username: e.target.value })
+        }
+      />
+    </label>
+  );
+}
+
+//double verify password
+function CreatePassword(profileData, setProfileData) {
+  return (
+    <div>
+      <label>
+        Password:
+        <input
+          type="password"
+          className="inputBox"
+          value={profileData.password}
+          onChange={(e) =>
+            setProfileData({ ...profileData, password: e.target.value })
+          }
+        />
+      </label>
+    </div>
+  );
+}
+
+function VerifyValidPassword(profileData, setProfileData) {
+  return (
+    <div>
+      <label>
+        Re-enter Password:
+        <input
+          type="password"
+          className="inputBox"
+          value={profileData.verifyPassword}
+          onChange={(e) =>
+            setProfileData({ ...profileData, verifyPassword: e.target.value })
+          }
+        />
+      </label>
+    </div>
+  );
+}
+
+function EnterFirstName(profileData, setProfileData) {
+  return (
+    <div>
+      <label>
+        First Name:
+        <input
+          type="text"
+          className="inputBox"
+          value={profileData.firstName}
+          onChange={(e) =>
+            setProfileData({ ...profileData, firstName: e.target.value })
+          }
+        />
+      </label>
+    </div>
+  );
+}
+
+function EnterLastName(profileData, setProfileData) {
+  return (
+    <div>
+      <label>
+        Last Name:
+        <input
+          type="text"
+          className="inputBox"
+          value={profileData.lastName}
+          onChange={(e) =>
+            setProfileData({ ...profileData, lastName: e.target.value })
+          }
+        />
+      </label>
+    </div>
+  );
+}
+
+// function EnterAge(profileData, setProfileData) {}
+
+function EnterDateOfBirth(profileData, setProfileData) {
+  return (
+    <div>
+      <label>
+        Date of Borth:
+        <input
+          type="text"
+          className="inputBox"
+          value={profileData.dateOfBirth}
+          onChange={(e) =>
+            setProfileData({ ...profileData, dateOfBirth: e.target.value })
+          }
+        />
+      </label>
+    </div>
+  );
+}
+
+function EnterEmail(profileData, setProfileData) {
+  return (
+    <div>
+      <label>
+        Email:
+        <input
+          type="text"
+          className="inputBox"
+          value={profileData.email}
+          onChange={(e) =>
+            setProfileData({ ...profileData, email: e.target.value })
+          }
+        />
+      </label>
+    </div>
+  );
+}
+
+function EnterPhoneNumber(profileData, setProfileData) {
+  return (
+    <div>
+      <label>
+        Phone Number:
+        <input
+          type="text"
+          className="inputBox"
+          value={profileData.phoneNumber}
+          onChange={(e) =>
+            setProfileData({ ...profileData, phoneNumber: e.target.value })
+          }
+        />
+      </label>
+    </div>
+  );
+}
+
+function CreateValidPassword(profileData, setProfileData) {
+  return (
+    <div>
+      {CreatePassword(profileData, setProfileData)}
+      {VerifyValidPassword(profileData, setProfileData)}
+    </div>
+  );
+}
+
 function ClearInputBoxes(setProfileData) {
   return setProfileData({
     firstName: "",
@@ -63,7 +217,12 @@ function SignUpForm() {
     <div className="signup-Page">
       <form className="signUp-Form" onSubmit={handleSubmit}>
         <h2>Create your profile</h2>
-        {/* //add in the paramter call */}
+        {EnterLastName(profileData, setProfileData)}
+        {EnterEmail(profileData, setProfileData)}
+        {EnterPhoneNumber(profileData, setProfileData)}
+        {EnterDateOfBirth(profileData, setProfileData)}
+        {CreateUsername(profileData, setProfileData)}
+        {CreateValidPassword(profileData, setProfileData)}
         <button type="submit">Create Account</button>
       </form>
     </div>
