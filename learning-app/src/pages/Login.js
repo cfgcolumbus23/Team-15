e9cededf9a5998c094b446353bbfdf2da48be5b8
Tina-help <tinalin036@gmail.com>
2023-11-04 01:16:25 -0400

    Attempted to do screen switching without button press

--- learning-app/src/pages/Login.js
+++ learning-app/src/pages/Login.js
@@ -1,20 +1,36 @@
 import "./Login.css";
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-// import { auth } from "../Connection"; // Assuming Connection.js is in the src folder, outside of the pages folder.
-// import { signInWithEmailAndPassword } from "firebase/auth"; // Corrected the import statement.
+import { auth } from "../Connection"; // Assuming Connection.js is in the src folder, outside of the pages folder.
+import { signInWithEmailAndPassword } from "firebase/auth"; // Corrected the import statement.
+import { useHistory } from "react-router-dom";
 
 //Call the backend to authenticate the login information
 //have it return a success or failure
 function AuthenticateLoginInformation(login) {
-  //return signInWithEmailAndPassword(auth, login.username, login.password);
+  return signInWithEmailAndPassword(auth, login.username, login.password);
 }
 
+const switchScreenToAssessement = () => {
+  // const history = useHistory();
+
+  // useEffect(() => {
+  //   const delay = setTimeout(() => {
+  //     history.push('/new-route');
+  //   }, 3000); // Redirect after 3 seconds (3000 milliseconds)
+
+  //   return () => clearTimeout(delay); // Clear timeout on unmount or re-render
+  // }, [history]);
+
+  return <div>{/* Your component's content */}</div>;
+};
+
 function HandleResult(success) {
-  if (success) {
+  if (true) {
     //switch to the new screen that says Success!
-    console.log("Success");
+    //Pull information regarding if the user completed the assessment
+    //for now assume they did not:
   } else {
     //display an error message
     console.log("Failure");
